3fbd45e410f65fd52abfcb568beae53ba82cdbc6
nazmulfci <nazmulfci@yahoo.com>
2023-09-20 23:33:47 +0600

    2nd

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue'
+ import {useRoute} from 'vue-router'
+
+const route = useRoute()
+const  profile = ref('Hi, This is Mr.'+ route.params.username)
+</script>
+
+<template>
+  <main>
+   <h1>{{profile}}  </h1>
+   <p>Nice to meet you.</p>
+  </main>
+</template>
+
+<style scoped >
+main{
+    background: #ddd;
+    border: 10px solid #41b883;
+    text-align: center;
+}
+</style>
